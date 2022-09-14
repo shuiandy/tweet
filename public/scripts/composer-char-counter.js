11dd945cde228faa17b5e4444f2aa4ec1db3bce1
Shuai Hu <shuiandy@gmail.com>
2022-09-14 10:10:46 -0400

    fixed typo

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,13 @@
 $(document).ready(function () {
   $('textarea').keyup(function () {
     let value = $(this).val().length;
-    let maximium = 140;
-    let currentLen = maximium - value;
-    console.log(currentLen);
+    let maximum = 140;
+    let currentLen = maximum - value;
+    $('.counter').text(currentLen);
     if (currentLen < 0) {
       $('.counter').css('color', 'red');
     } else {
       $('.counter').css('color', '#555149');
     }
-    $('.counter').text(currentLen);
-    console.log(value);
   });
 });
